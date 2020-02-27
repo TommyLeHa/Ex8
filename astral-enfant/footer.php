e2f3ae9288b83fa5ec698798828fa0d3b547f396
Tommy Le Ha <leha.tommy.pro@gmail.com>
2020-02-27 17:58:40 -0500

    Modification du footer.php

--- astral-enfant/footer.php
+++ astral-enfant/footer.php
@@ -2,35 +2,35 @@
 /**
  * The template for displaying the footer
  *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Underscores
+ * @package Astral
+ * @since 0.1
  */
-
 ?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				//printf( esc_html__( 'Proudly powered by %s', 'underscores' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores' ), 'underscores', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+<footer class="footer py-md-5 pt-md-3 pb-sm-5">
+	<?php
+	/* 
+	* Functions hooked into astral_footer_widget_area action
+	*
+	* @hooked astral_footer_widget
+	*/
+	do_action( 'astral_footer_widget_area' );
+	?>
+</footer>
+<div class="cpy-right text-center mwa-theme">
+	<?php
+	/* 
+	* Functions hooked into astral_copyright_area action
+	*
+	* @hooked astral_copyright
+	*/
+	do_action( 'astral_copyright_area' );
+	?>
+</div>
+<!-- scroll top icon -->
+<a href="#" id="scroll" class="move-top text-center scrollup" style="">
+    <div class="circle"><i class="fa fa-chevron-up" aria-hidden="true"></i></div>
+</a>
+<?php get_template_part('google', 'font'); ?>
 <?php wp_footer(); ?>
-
 </body>
 </html>
